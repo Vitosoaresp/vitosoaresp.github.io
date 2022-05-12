dc9f81541a448710fee7caa776c1435a6a12b3d7
Vitor Soares <pereiravitor1218@gmail.com>
2022-05-11 23:24:13 -0300

    algumas alterações no componente Projetos

--- src/componentes/Projetos.jsx
+++ src/componentes/Projetos.jsx
@@ -24,7 +24,7 @@ class Projetos extends React.Component {
               </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="1">
-              <Accordion.Header>Accordion Item #2</Accordion.Header>
+              <Accordion.Header>Em Breve</Accordion.Header>
               <Accordion.Body>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
