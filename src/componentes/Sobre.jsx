933fe13dfe23b8844e1a18088369bad253022bfe
Vitor Soares <pereiravitor1218@gmail.com>
2022-05-17 16:18:55 -0300

    muda o background e o button do github

--- src/componentes/Sobre.jsx
+++ src/componentes/Sobre.jsx
@@ -5,15 +5,23 @@ import './Sobre.css';
 class Sobre extends React.Component {
   render() {
     return (
-      <div className="sobre-container" id="sobre">
+      <div className='sobre-container' id='sobre'>
         <p>Bem Vindo ao meu Portfolio!</p>
-        <p><strong>VENHA ME CONHECER</strong></p>
-        <div className="sobre-button-container" id="sobre">
-          <a href="#habilidades">
-            <Button color="primary">CONHECER</Button>
+        <p>
+          <strong>VENHA ME CONHECER</strong>
+        </p>
+        <div className='sobre-button-container' id='sobre'>
+          <a href='#habilidades'>
+            <Button color='primary'>CONHECER</Button>
           </a>
-          <a href="https://github.com/Vitosoaresp" target="_blank" rel="noopener noreferrer">
-            <Button color="dark">MEU GITHUB</Button>
+          <a
+            href='https://github.com/Vitosoaresp'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <button type='button' className='btn-github'>
+              MEU GITHUB
+            </button>
           </a>
         </div>
       </div>
