5349e6f005d125fcb0def80266b27f85d872dff0
Vitor Soares <pereiravitor1218@gmail.com>
2022-06-10 23:04:32 -0300

    muda app para componente funcional e deleta css dele

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from './componentes/Header';
-import Sobre from './componentes/Sobre';
-import Habilidades from './componentes/Habilidades';
-import Projetos from './componentes/Projetos';
+import React from 'react';
+import Header from './componentes/Header/Header';
+import Sobre from './componentes/Sobre/Sobre';
+// import Habilidades from './componentes/Habilidades';
+// import Projetos from './componentes/Projetos';
 import Contato from './componentes/Contato';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './componentes/Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <Sobre />
-        <Habilidades />
-        <Projetos />
-        <Contato />
-        <Footer />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Header />
+      <Sobre />
+      {/* <Habilidades /> */}
+      {/* <Projetos /> */}
+      <Contato />
+      <Footer />
+    </div>
+  );
 }
+
 
 export default App;
